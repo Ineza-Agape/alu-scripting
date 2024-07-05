e0e75ea564fe6ac1f9983aed425dbbc868c79d15
Ineza-Agape <i.agape@alustudent.com>
2024-07-05 13:44:32 +0000

    API

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""
-script that returns the number of subscribers (not active users, total subscribers) for a given subreddit
-"""
+"""Return the number of subscribers of a given subreddit"""
+
+import requests
 
 
 def number_of_subscribers(subreddit):
-    import requests
-    req = requests.get("https://api.reddit.com/r/{}/about.json"
-                       .format(subreddit),
-                       headers={"User-Agent": "MyRedditBot/0.0.1"})
-    req = req.json()
+    """function that fetches number_of_subscribers"""
+    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+
     try:
-        return req["data"]["subscribers"]
-    except Exception as error:
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        return RESPONSE.json().get("data").get("subscribers")
+
+    except Exception:
         return 0
